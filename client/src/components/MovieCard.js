deec8c5118899066d0aaa1c63548f98ebfbc417b
Vanessa Lane <vlane@23andme.com>
2020-10-30 10:45:02 -1000

    move event handlers to SearchedMovies.js

---
+++ client/src/components/MovieCard.js
@@ -0,0 +1,102 @@
+import React from 'react';
+
+// import bootstrap-react components
+import { Accordion, Button, Card, ResponsiveEmbed } from 'react-bootstrap';
+import StarRatings from 'react-star-ratings';
+
+// import utils
+import Auth from '../utils/auth';
+import { useFantinderContext } from "../utils/GlobalState";
+
+const MovieCard = (props) => {
+    const [state, ] = useFantinderContext();
+    const { likedMovies, dislikedMovies } = state;
+    const {
+        movie,
+        displayTrailer,
+        likeMovieHandler,
+        dislikeMovieHandler,
+        skipMovieHandler,
+        displaySkip
+    } = props;
+
+    return (
+        <Accordion>
+            <Card>
+                {displayTrailer && movie.trailer
+                    ? <ResponsiveEmbed aspectRatio="16by9">
+                        <iframe
+                            title={movie._id}
+                            width="560"
+                            height="315"
+                            src={movie.trailer}
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                    </ResponsiveEmbed>
+                    : (movie.poster && <Card.Img src={movie.poster} alt={`The cover for ${movie.title}`} variant='top' />)
+                }
+                <Card.Body>
+                    <Card.Title>
+                      {movie.title}
+                    </Card.Title>
+                        <StarRatings
+                            rating={movie.rating/2}
+                            numberOfStars={5}
+                            name={`${movie._id}-rating`}
+                            starDimension="20px"
+                            starSpacing="1px"
+                        />
+
+                        <Card.Text className='small'>
+                            ({movie.voteCount?.toLocaleString()} ratings)
+                        </Card.Text>
+                    <Accordion.Toggle className="small" as={Card.Link} variant="link" eventKey={movie._id}>
+                        Click to expand for more details
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey={movie._id}>
+                        <Card.Body>
+                            <Card.Text>Plot Summary</Card.Text>
+                            <Card.Text className='small'>{movie.overview}</Card.Text>
+                            <Card.Text className='small'>Release Date: {movie.releaseDate}</Card.Text>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card.Body>
+                    {Auth.loggedIn()
+                    ?   
+                        <Card.Footer className="d-flex justify-content-between">
+                            <Button
+                                className="movie-card-button"
+                                disabled={dislikedMovies?.some(dislikedMovie => dislikedMovie._id === movie._id)}
+                                variant={dislikedMovies?.some(dislikedMovie => dislikedMovie._id === movie._id) ? "outline-secondary" : "outline-danger"}
+                                onClick={() => dislikeMovieHandler(movie)}>
+                                    {dislikedMovies?.some(dislikedMovie => dislikedMovie._id === movie._id)
+                                    ? <span>Disliked!</span>
+                                    : <i className='far fa-thumbs-down fa-2x' />}
+                            </Button>
+                            <Button
+                                className="movie-card-button"
+                                disabled={likedMovies?.some(likedMovie => likedMovie._id === movie._id)}
+                                variant={likedMovies?.some(likedMovie => likedMovie._id === movie._id) ? "outline-secondary" : "outline-success"}
+                                onClick={() => likeMovieHandler(movie)}>
+                                    {likedMovies?.some(likedMovie => likedMovie._id === movie._id)
+                                    ? <span>Liked!</span>
+                                    : <i className='far fa-thumbs-up fa-2x' />}
+                            </Button>
+                        </Card.Footer>
+                    : displaySkip &&
+                        <Card.Footer className="text-center">
+                            <Button
+                                className="movie-card-button"
+                                size="lg"
+                                onClick={() => skipMovieHandler(movie._id)}>
+                                    Next Movie
+                            </Button>
+                        </Card.Footer>
+                    }
+            </Card>
+        </Accordion>
+    )
+}
+
+export default MovieCard;
--- client/src/pages/SearchMovies.js
+++ client/src/pages/SearchMovies.js
@@ -1,28 +1,76 @@
-import React, { useState } from 'react';
-
-// import TMDB API dependencies
-import { searchTMDB } from '../utils/API';
-
-// import GraphQL Dependencies
-import { ADD_MOVIE } from '../utils/mutations';
-import { useMutation } from '@apollo/react-hooks';
-
-// import react-bootstrap components
-import { Form, Button, Container, Jumbotron } from 'react-bootstrap';
-
-// import custom components
-import MovieCards from '../components/MovieCards'
+import React, { useEffect, useState } from 'react';
+// Components
+import { Form, Button, CardColumns, Container, Jumbotron } from 'react-bootstrap';
+import MovieCard from '../components/MovieCard'
 import { cleanMovieData } from '../utils/movieData';
+// TMDB API
+import { searchTMDB } from '../utils/API';
+// GraphQL
+import { ADD_MOVIE, DISLIKE_MOVIE, LIKE_MOVIE } from '../utils/mutations';
+import { GET_USER } from '../utils/queries';
+import { useMutation, useQuery } from '@apollo/react-hooks';
+// Context API
+import { useFantinderContext } from "../utils/GlobalState";
+import { UPDATE_DISLIKED_MOVIES, UPDATE_LIKED_MOVIES } from '../utils/actions';
+// indexedDB
+import { idbPromise } from "../utils/helpers";
 
 const SearchMovies = () => {
+    // state management
+    const [state, dispatch] = useFantinderContext();
     const [searchInput, setSearchInput] = useState('');
     const [noResultsFound, setNoResultsFound] = useState(false);
     const [searchedMovies, setSearchedMovies] = useState([]);
     const [searching, setSearching] = useState(false);
+    // graph ql
     const [addMovie, { addMovieError }] = useMutation(ADD_MOVIE);
+    const [dislikeMovie, { dislikeError }] = useMutation(DISLIKE_MOVIE);
+    const [likeMovie, { likeError }] = useMutation(LIKE_MOVIE);
+    const { loading, data } = useQuery(GET_USER);
+
+    // get the movie preferences for the current user to handle like/dislike functionality
+    useEffect(() => {
+        if (data && data.me) {
+            dispatch({
+                type: UPDATE_DISLIKED_MOVIES,
+                dislikedMovies: data.me.dislikedMovies
+            })
+
+            dispatch({
+                type: UPDATE_LIKED_MOVIES,
+                likedMovies: data.me.likedMovies
+            })
+
+            data.me.dislikedMovies.forEach((movie) => {
+                idbPromise('dislikedMovies', 'put', movie);
+                idbPromise('likedMovies', 'delete', movie);
+            });
+    
+            data.me.likedMovies.forEach((movie) => {
+                idbPromise('dislikedMovies', 'delete', movie);
+                idbPromise('likedMovies', 'put', movie);
+            });
+        } else if (!loading) {
+            idbPromise('dislikedMovies', 'get').then(dislikedMovies => {
+              dispatch({
+                type: UPDATE_DISLIKED_MOVIES,
+                dislikedMovies: dislikedMovies
+              });
+            });
+
+            idbPromise('likedMovies', 'get').then(likedMovies => {
+              dispatch({
+                type: UPDATE_LIKED_MOVIES,
+                likedMovies: likedMovies
+              });
+            });
+          }
+    }, [data, loading, dispatch]);
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
+
+        setSearchedMovies([]);
         setSearching(true);
 
         if (!searchInput) {
@@ -30,17 +78,20 @@ const SearchMovies = () => {
         }
 
         const response = await searchTMDB(searchInput);
-        
+
         if (!response.ok) {
             throw new Error("Couldn't search for movies");
         }
 
         const { results } = await response.json();
-        if (results.length > 0) {
-            setNoResultsFound(false);
-        } else {
+
+        // return early if no results found
+        if (results.length === 0) {
             setNoResultsFound(true);
+            setSearching(false);
+            return;
         }
+
         const cleanedMovies = await cleanMovieData(results);
 
         const updatedSearchedMovies = [];
@@ -56,9 +107,70 @@ const SearchMovies = () => {
                 updatedSearchedMovies.push(data.addMovie);
             }
         };
+
         setSearchedMovies(updatedSearchedMovies);
         setSearching(false);
+        setNoResultsFound(false);
+    };
 
+    const handleLikeMovie = async (likedMovie) => {
+        try {
+            // update the db
+            let { data } = await likeMovie({
+                variables: { movieId: likedMovie._id }
+            });
+
+            // throw an error if the mutation failed
+            if (likeError) {
+                throw new Error('Something went wrong!');
+            }
+
+            // update global state
+            dispatch({
+                type: UPDATE_LIKED_MOVIES,
+                likedMovies: data.likeMovie.likedMovies
+            });
+            dispatch({
+                type: UPDATE_DISLIKED_MOVIES,
+                dislikedMovies: data.likeMovie.dislikedMovies
+            });
+
+            // update idb
+            idbPromise('likedMovies', 'put', likedMovie);
+            idbPromise('dislikedMovies', 'delete', likedMovie);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleDislikeMovie = async (dislikedMovie) => {
+        try {
+            // update the db
+            let { data } = await dislikeMovie({
+                variables: { movieId: dislikedMovie._id }
+            });
+
+            // throw an error if the mutation failed
+            if (dislikeError) {
+                throw new Error('Something went wrong!');
+            }
+
+            // update global state
+            dispatch({
+                type: UPDATE_LIKED_MOVIES,
+                likedMovies: data.dislikeMovie.likedMovies
+            });
+            dispatch({
+                type: UPDATE_DISLIKED_MOVIES,
+                dislikedMovies: data.dislikeMovie.dislikedMovies
+            });
+
+            // update idb
+            idbPromise('likedMovies', 'put', dislikedMovie);
+            idbPromise('dislikedMovies', 'delete', dislikedMovie);
+        } catch (err) {
+            console.error(err);
+        }
     };
 
     return (
@@ -85,7 +197,19 @@ const SearchMovies = () => {
                             <h2 className="results-heading">
                                 {searchedMovies.length > 0 && `Viewing ${searchedMovies.length} results:`}
                             </h2>
-                            <MovieCards displayTrailers moviesToDisplay={searchedMovies} />
+                            <CardColumns>
+                            {searchedMovies?.map(movie => {
+                                return (
+                                    <MovieCard
+                                        key={movie._id}
+                                        movie={movie}
+                                        displayTrailer
+                                        likeMovieHandler={handleLikeMovie}
+                                        dislikeMovieHandler={handleDislikeMovie}
+                                    />
+                                )
+                            })}
+                            </CardColumns>
                         </>
                 }
             </Container>
